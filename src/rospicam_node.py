4049125ecc6b87bdd77e8b0b913479813de4cb89
pico <pico737@gmail.com>
2016-11-18 15:05:42 -0800

    initial commit

---
+++ src/rospicam_node.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from picamera import PiCamera
+from io import BytesIO
+
+# ros imports
+import rospy
+from std_msgs.msg import Header
+from sensor_msgs.msg import CompressedImage
+
+class RosPiCam:
+    def __init__(self):
+        print "rospicam start"
+
+        # publishers
+        self.image_pub = rospy.Publisher('/rospicam/image/compressed', CompressedImage, queue_size=10)
+
+        # init the node
+        rospy.init_node('rospicam', anonymous=True)
+
+        stream = BytesIO()
+        camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 30
+        
+        for foo in camera.capture_continuous(stream, format='jpeg'):
+            stream.truncate()
+            stream.seek(0)
+
+            publish_image(stream)
+
+            if rospy.is_shutdown():
+                break
+
+        stream.close()
+        camera.close()
+
+        print "rospicam stop"
+
+    def publish_image(self, stream):
+        image_msg = sensor_msgs.CompressedImage()
+        image_msg.header = Header()
+        image_msg.format = 'jpeg'
+        image_msg.data = stream.getvalue()
+
+        self.image_pub.publish(image_msg)
+        return rospy.is_shutdown()
+
+
+if __name__ == '__main__':
+    try:
+        RosPiCam()
+    except rospy.ROSInterruptException:
+        pass
